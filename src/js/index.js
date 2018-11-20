f7c1e02b9734be2fc3d686229bc4fc0cfc7da182
Soar <soar.lin@gmail.com>
2018-11-20 23:06:23 +0800

    Add sidebar for switch scene content

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,27 @@
+// 載入圖片檔
+import '../images/*.*'
+
 // import jquery
 import './import-jquery.js'
+
+// 載入 Semantic-UI css
+import '../semantic/dist/semantic.min.css'
 
 // 載入 style
 import '../sass/style.sass'
 
-// 載入圖片檔
-import '../images/*.*'
-
-// 載入 Semantic-UI css
-import '../semantic/dist/semantic.min.css'
 // load Semantic-UI js
 import '../semantic/dist/semantic.min.js'
+
+$('.js-sidebar')
+  .sidebar({
+    context: '#mainContainer'
+  })
+  .sidebar('setting', {
+    dimPage: false,
+    closable: false,
+    transition: 'overlay'
+  })
 
 var mySwiper = new Swiper('.swiper-container', {
   // Optional parameters
@@ -30,6 +41,8 @@ $('.js-scenes-table td').on('click', function (e) {
   let sceneId = $(target).data('id')
   console.log('清除 mySwiper 內所有圖片')
   console.log(`載入場景${sceneId}的圖片`)
+  console.log('sidebar內換成場景相關內容')
+  $('.ui.sidebar').sidebar('toggle')
 })
 
 function dragStart(e) {
